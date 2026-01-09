6f0e0a06d9b0736041fa08f4482b894bb3dde8ee
elsadiq <sadig@gmx.ch>
2026-01-09 21:29:14 +0100

    changes:
    - implementing productlist component

--- src/app/components/productlist-component/productlist-component.ts
+++ src/app/components/productlist-component/productlist-component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ProductService } from '../../services/product-service';
+import { Product } from '../../Models/product';
+import { Subscription } from 'rxjs';
+import { ProductComponent } from '../product-component/product-component';
 
 @Component({
   selector: 'app-productlist-component',
-  imports: [],
+  imports: [ProductComponent],
   templateUrl: './productlist-component.html',
   styleUrl: './productlist-component.scss',
 })
 export class ProductlistComponent {
+
+  protected products = signal<Product[]>([]);
+  private getproductssubscription!: Subscription;
+  private productService = inject(ProductService);
+
+  ngOnInit(): void {
+    this.getproductssubscription = this.productService.getProducts().subscribe((resp) => {
+      this.products.set(resp);
+    });
+  }
+  addProduct() {
+
+  }
+  ngOnDestroy(): void {
+    this.getproductssubscription.unsubscribe();
+  }
+
 
 }
