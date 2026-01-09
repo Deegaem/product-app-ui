f0d48546fb86e2f17d0d81c6b9ecc5d87e8c7086
elsadiq <sadig@gmx.ch>
2026-01-09 20:13:35 +0100

    changes:
    - implementing product-component

--- src/app/components/product-component/product-component.ts
+++ src/app/components/product-component/product-component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
+import { ProductService } from '../../services/product-service';
+import { Product } from '../../Models/product';
 
 @Component({
   selector: 'app-product-component',
@@ -7,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrl: './product-component.scss',
 })
 export class ProductComponent {
+
+  product = input.required<Product>();
+
+  private productService = inject(ProductService);
+
+  ngOnInit(): void {
+
+  }
+
+  productDetails(id: number) {
+  }
 
 }
